08bd494e50954d4ff86463ecba1ec134fa714be6
Benvinda Vedes <bafonso.vedes@gmail.com>
2020-10-19 18:34:02 +0100

    Updates

---
+++ precache-manifest.d4670c3e610f6173974f75f5935180e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aacf318e22e7767632f57d61f1ff50c",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "d1973e4aa7aa50ea0030",
+    "url": "/random-quote-machine/static/css/main.145fed4e.chunk.css"
+  },
+  {
+    "revision": "e9b222c254502dea1264",
+    "url": "/random-quote-machine/static/js/2.909f7adf.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/random-quote-machine/static/js/2.909f7adf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1973e4aa7aa50ea0030",
+    "url": "/random-quote-machine/static/js/main.10997ab7.chunk.js"
+  },
+  {
+    "revision": "79be2feea42a5a087d5f",
+    "url": "/random-quote-machine/static/js/runtime-main.630878ba.js"
+  }
+]);
